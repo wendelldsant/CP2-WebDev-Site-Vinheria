644a320edbdb2b02389d417ce1c9f9f1f3d513e1
wendelldsant <wendell_dsantos@hotmail.com>
2024-04-19 19:07:32 -0300

    script.js_add

---
+++ script.js
@@ -0,0 +1,8 @@
+function usuario() {
+    var login = parseFloat(document.getElementById("login").value);
+    var senha = parseFloat(document.getElementById("senha").value);
+    if (login === 1234 && senha === 1234){
+        alert("Ola")
+        // window.open("http://127.0.0.1:5500/+18.html") --> função para abrir nova guia
+    }
+}
