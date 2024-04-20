5ea81473e32a66727103f083f3eddcc94aa0323c
wendelldsant <wendell_dsantos@hotmail.com>
2024-04-20 11:20:44 -0300

    add_button_idade_script

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
+function maior_redirecionar() {
+        window.open("+18.html") // função para abrir guia do +18
+    }
+function menor_redirecionar() {
+        window.open("-18.html") // função para abrir guia do +18
+    }
 function usuario() {
     var login = parseFloat(document.getElementById("login").value);
     var senha = parseFloat(document.getElementById("senha").value);
     if (login === 1234 && senha === 1234){
         alert("Ola")
-        window.open("http://127.0.0.1:5500/+18.html") --> função para abrir nova guia
+        window.open("http://127.0.0.1:5500/+18.html")
     }
+
 }
+
