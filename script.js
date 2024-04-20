bdd660df039ffd907dfe12b1e4f7ffd295504198
wendelldsant <wendell_dsantos@hotmail.com>
2024-04-20 13:10:59 -0300

    login_changes

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
 function maior_redirecionar() {
-        window.open("+18.html") // função para abrir guia do +18
-    }
-function menor_redirecionar() {
-        window.open("-18.html") // função para abrir guia do +18
-    }
-function usuario() {
-    var login = parseFloat(document.getElementById("login").value);
-    var senha = parseFloat(document.getElementById("senha").value);
-    if (login === 1234 && senha === 1234){
-        window.open("vinhos_opcoes.html")
-    }
-
+    window.open("+18.html"); // função para abrir guia do +18
 }
 
+function menor_redirecionar() {
+    window.open("-18.html"); // função para abrir guia do -18
+}
+
+function usuario() {
+    var login = document.getElementById("login").value;
+    var senha = document.getElementById("senha").value;
+
+    if (login == "1234" && senha == "1234") {
+        window.open("vinhos_opcoes.html");
+    } else {
+        alert('Usuário e Senha não registrados. Tente novamente.');
+    }
+}
