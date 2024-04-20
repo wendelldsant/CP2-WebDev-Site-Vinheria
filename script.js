c94bbc6f38b29d26052030b89c9f15d36ffa1532
wendelldsant <wendell_dsantos@hotmail.com>
2024-04-20 11:48:37 -0300

    redirecionamento_criacao_page_vinhos

--- script.js
+++ script.js
@@ -8,8 +8,7 @@ function usuario() {
     var login = parseFloat(document.getElementById("login").value);
     var senha = parseFloat(document.getElementById("senha").value);
     if (login === 1234 && senha === 1234){
-        alert("Ola")
-        window.open("http://127.0.0.1:5500/+18.html")
+        window.open("vinhos_opcoes.html")
     }
 
 }
